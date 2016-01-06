48e200cfb4b24e709b7502f0e0576cf8415d4713
Fengtalk <achisky@gmail.com>
2016-01-06 21:37:12 +0800

    Update Plugin.php
    小修改

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * 原项目地址<a href="https://github.com/abelyao/Typecho-QiniuFile" target="_blank">在此</a>，年久失修，不能正常在 Typecho 1.0 上使用，经由<a href="https://github.com/MarlonFan/qiniu-typecho-file" target="_blank">MarlonFan</>修改，得以正常。表示感谢！ 
+ * 原项目地址<a href="https://github.com/abelyao/Typecho-QiniuFile" target="_blank">在此</a>，不能正常在 Typecho 1.0 上使用。
+ * 经由<a href="https://github.com/MarlonFan/qiniu-typecho-file" target="_blank">MarlonFan</a>修改，得以正常。在此表示感谢！ 
  * 下载后，请将文件夹名称修改为「QiniuFile」，否则可能无法正常使用。
  * @package Qiniu File
  * @author abelyao
